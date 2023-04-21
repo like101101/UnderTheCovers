d5a13b1de71736470a4019315550a1932b31fad0
like101101 <likelike101101@gmail.com>
2023-04-21 19:13:52 -0400

    Added production-level workflow

---
+++ tests/assignment-test/testassignments.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+set -e 
+
+
+# Variables for color prints
+Color_Off='\033[0m'       # Text Reset
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+
+# Configuration
+if [ -z $1 ]; then
+    echo "  ---- Running inside test image ----"
+    RUN_LOCALLY=false
+    mkdir -p /autograder/source
+    TESTING_LOC="/autograder/source"
+else    
+    echo "  ---- Running locally ----"
+    RUN_LOCALLY=true
+    mkdir -p tmp
+    TESTING_LOC="tmp"
+fi
+
+# Iterating through all assignment directory
+for PS_DIR in PS*; do
+    echo "  ---- Testing ${PS_DIR} ----"
+
+    # Ensure that the autograder scripts are executable
+    chmod +x $PS_DIR/run_autograder
+
+    # copying the autograder scripts to the write place
+    cp -r $PS_DIR/* $TESTING_LOC
+    cp read_results.py $TESTING_LOC
+    
+
+    # Run the autograder scripts
+    cd ${TESTING_LOC}
+    make
+    ./run_autograder > output.log 2>&1
+    score=$(python read_results.py)
+
+    # Check the results
+    if [ $score -ne 80 ]; then
+        cat output.log
+        printf "${Red}  ---- Test Failed! ---- ${Color_Off}\n"
+        exit 1
+    fi
+
+    
+    # Clean Up
+    rm -f output.log
+    rm -rf ${TESTING_LOC}/*
+    cd ~
+
+done
+
+echo 
+printf "${Green}  ---- All Tests Passed! ---- ${Color_Off}\n"
+echo
